bd767459d0cd4f2dde8d0baa931689de8df370e1
Britney Wright <blw06g@gmail.com>
2014-11-26 01:50:33 -0500

    copy tests

--- app/models/copy.rb
+++ app/models/copy.rb
@@ -1,4 +1,8 @@
 class Copy < ActiveRecord::Base
   belongs_to :book
   has_many :statuses
+
+  validates :copy_number, presence: true, numericality: true
+
+  validates_associated :book
 end
--- spec/models/copy_spec.rb
+++ spec/models/copy_spec.rb
@@ -6,6 +6,14 @@ describe Copy do
     expect(build(:copy)).to be_valid
   end
 
+  it "is invalid without a copy_number" do
+    expect(build(:copy, copy_number: nil)).to_not be_valid
+  end
+
+  it "is invalid unless copy_number is an integer" do
+    expect(build(:copy, copy_number: "food")).to_not be_valid
+  end
+
   it "belongs to a book" do
     something = create(:book)
     expect(build(:copy, book: something).book).to be something
