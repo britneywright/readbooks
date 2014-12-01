3c558b6ec7b55e94a40ef358e808dc718baa530a
Britney Wright <blw06g@gmail.com>
2014-12-01 00:21:36 -0500

    new mailers

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,8 +1,13 @@
 class ReservationMailer < ActionMailer::Base
-  default from: "blw06g@gmail.com"
+  default from: "britney@britneywright.com"
   
   def confirmation_email(status)
     @status = status
     mail(to: @status.borrower_email, subject: "Rails Girls DC Library Request Received")
+  end
+
+  def request_email(status)
+    @status = status
+    mail(to: "blw06g@gmail.com", subject: "Rails Girls DC Library Request Received")
   end
 end
